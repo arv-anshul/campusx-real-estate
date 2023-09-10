ff9cfa9fb05109f0b08525758981d7572384b811
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-09-11 02:17:14 +0530

    Add SchemaReader class

---
+++ src/database/reader.py
@@ -0,0 +1,22 @@
+import json
+from typing import Self
+
+from src.typing import CAT_COLS_Key, PropertyAlias
+
+
+class SchemaReader:
+    _instance: dict[PropertyAlias, Self] = {}
+
+    def __new__(cls, prop_type: PropertyAlias):
+        if prop_type not in cls._instance:
+            cls._instance[prop_type] = super(SchemaReader, cls).__new__(cls)
+            cls._instance[prop_type]._load_schema(prop_type)
+        return cls._instance[prop_type]
+
+    def _load_schema(self, prop_type: PropertyAlias):
+        schema_dict = json.load(open("src/database/schema.json"))[prop_type]
+
+        self.TARGET: str = schema_dict["target"]
+        self.ALL_COLS: list[str] = schema_dict["all_cols"]
+        self.NUM_COLS: list[str] = schema_dict["num_cols"]
+        self.CAT_COLS: dict[CAT_COLS_Key, list[str]] = schema_dict["cat_cols"]
