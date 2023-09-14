08963337498791531efb851a330cb755b28e0b70
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-09-14 16:18:03 +0530

    ⬆️ Update src.property.entity.property_type

--- src/property/entity/property_type.py
+++ src/property/entity/property_type.py
@@ -1,13 +1,8 @@
 from abc import ABC, abstractmethod
 
-from src.database.schema_reader import SchemaReader
-
 
 class PropertyType(ABC):
     """Abstract class for to for different property type in real estate."""
-
-    st_form_key: str
-    schema: SchemaReader
 
     @abstractmethod
     def st_form(cls) -> None:
