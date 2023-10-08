031d7d453e14614cf5b51f131dcff26723306f81
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-10-08 18:48:58 +0530

    💄 Update app's homepage script

--- Real_Estate_Project.py
+++ Real_Estate_Project.py
@@ -1,11 +1,13 @@
 """ README page for Real Estate Project. """
 
+from pathlib import Path
+
 import streamlit as st
 
-st.set_page_config("README.md", "🗒️", "wide")
+README_PATH = Path("README.md")
+st.set_page_config("README.md", "📝", "wide")
 
 try:
-    with open("README.md") as md:
-        st.markdown(md.read())
+    st.markdown(README_PATH.read_text())
 except FileNotFoundError:
     st.error("README.md file not found.", icon="🔥")
